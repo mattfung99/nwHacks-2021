03c51bedcbe4945f83e2ba48fce4880fcbecaccc
mfung <official.matthiasfung@gmail.com>
2021-01-09 17:45:57 -0800

    Fix conflicts

--- recycle-calculator/src/index.js
+++ recycle-calculator/src/index.js
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const IndexTitle = "Welcome to the Recycling Calculator"
 const ButtonBegin = "Begin"
 
-<<<<<<< HEAD
 function CreateWelcomePage() {
     return (
         <section>
@@ -22,10 +21,3 @@ function CreateWelcomePage() {
 }
 
 ReactDom.render(<CreateWelcomePage />, document.getElementById('root'));
-=======
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
- 
->>>>>>> dab330db89a1b177ab02ccfc1844aa936655749d
